4dfa3dfaa83db22d651b9ef52e114178a0b16d10
Priya <Priyamadiwalar0007@gmail.com>
2022-02-15 21:59:22 +0530

    Added: Compare To Method

--- src/com/blz/LineComparison/LineComparison.java
+++ src/com/blz/LineComparison/LineComparison.java
@@ -4,14 +4,14 @@ package com.blz.LineComparison;
 
    public class LineComparison {
 	   public static int x1,x2,y1,y2,x3,x4,y3,y4;
-       public static void main(String[] args) {
+		public static void main(String[] args) {
 			getInput();
 			CalculateLength cal = new CalculateLength();
 			cal.calLength();
-			CheckEquality check = new CheckEquality();
-			check.checkEquality();
+			CompareLength compare = new CompareLength();
+			compare.checkEquality();
 		}
-	  public static void getInput() {
+		public static void getInput() {
 			Scanner sc = new Scanner(System.in);
 			System.out.println("For given line");
 			System.out.println("Enter co-ordinates of x1 and y1");
@@ -29,25 +29,27 @@ package com.blz.LineComparison;
 			y4 = sc.nextInt();
 		}
 	}
-class CalculateLength{
-	   static Double length1,length2;
-	   public void calLength() {
+	class CalculateLength{
+			static Double length1,length2;
+			public void calLength() {
 			    length1 = Math.sqrt((LineComparison.x2 - LineComparison.x1) * (LineComparison.x2 - LineComparison.x1) + (LineComparison.y2 - LineComparison.y1) * (LineComparison.y2 - LineComparison.y1));
 				System.out.println("Length of first line = "+length1+" units");
 			    length2 = Math.sqrt((LineComparison.x4 - LineComparison.x3) * (LineComparison.x4 - LineComparison.x3) + (LineComparison.y4 - LineComparison.y3) * (LineComparison.y4 - LineComparison.y3));
 				System.out.println("Length of second line = "+length2+" units");
 			}
 	}
-class CheckEquality{
-	  public  void checkEquality() {
-				if(CalculateLength.length1.equals(CalculateLength.length2)) {
-					System.out.println("Both lines are EQUAL in length");
+	class CompareLength{
+			public  void checkEquality() {
+				if(CalculateLength.length1.compareTo(CalculateLength.length2) == 1) {
+					System.out.println("Line1 is greater than line2 in length");
+				}
+				else if (CalculateLength.length1.compareTo(CalculateLength.length2) == -1) {
+					System.out.println("Line1 is less than line2 in length");
 				}
 				else {
-					System.out.println("Both lines are NOT EQUAL in length");
+					System.out.println("Both lines are EQUAL in length");
 				}
 			}
 	}
-
    
-	    
+	   
